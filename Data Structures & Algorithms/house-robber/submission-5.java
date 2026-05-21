6dc722aca02ec22db2d7b8aab5127f7c0009bbe1
Varun-tej-reddy <pvaruntejreddy@gmail.com>
2026-05-21 22:58:53 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,37 @@
+/*
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp,-1);
+
+        return helper(nums,dp,nums.length-1);
+    }
+    private static int helper(int[] nums, int[] dp, int n){
+        if(n < 0) return 0;
+        if( n == 0) return nums[0];
+
+        if(dp[n] != -1) return dp[n];
+
+        int a = nums[n] + helper(nums,dp,n-2);
+        int b = helper(nums,dp,n-1);
+
+        dp[n] = Math.max(a,b);
+        return dp[n];
+    }
+}
+*/
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for(int i=0;i<nums.length;i++){
+            int tem = rob1;
+            rob1 = Math.max(rob2+nums[i],rob1);
+            rob2 = tem;
+        }
+        return rob1;
+    }
+}
