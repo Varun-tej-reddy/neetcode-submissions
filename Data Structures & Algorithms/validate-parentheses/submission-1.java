3d0408a9545a37a3b428ab89c510212f142a14de
Varun-tej-reddy <pvaruntejreddy@gmail.com>
2026-07-05 21:57:04 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                if(!st.isEmpty()){
+                    if(st.peek() == '(' && c == ')')st.pop();
+                    else if(st.peek() == '{' && c == '}')st.pop();
+                    else if(st.peek() == '[' && c == ']')st.pop();
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
